00b4484fc50ca11bb518491ca1c69725221923bf
msan <50791439+xowlsakffl@users.noreply.github.com>
2025-06-21 21:23:03 +0900

    project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold'>
+      <h2 className='font-notosans'>welcome 가나다라</h2>
+      <FaBeer />
+    </div>
+  )
+}
+
+export default App
